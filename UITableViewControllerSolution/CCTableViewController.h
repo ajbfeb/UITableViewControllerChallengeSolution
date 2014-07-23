bf094c7a4ae3ba4bb2f86fbac7d5cd0bc4cdfaf4
Alejandro Jerez <ajb20109@Alejandros-MacBook-Pro.local>
2014-07-23 17:59:57 -0400

    solved the challenge

--- UITableViewControllerSolution/CCTableViewController.h
+++ UITableViewControllerSolution/CCTableViewController.h
@@ -1,5 +1,5 @@
 //
-//  CCViewController.h
+//  CCTableViewController.h
 //  UITableViewControllerSolution
 //
 //  Created by Alejandro Jerez on 7/23/14.
@@ -8,6 +8,6 @@
 
 #import <UIKit/UIKit.h>
 
-@interface CCViewController : UIViewController
+@interface CCTableViewController : UITableViewController
 
 @end
